ed2b43b8a617645acca68cd1da0730879ba7c5e1
roctbb <roctbb@gmail.com>
2021-11-11 14:34:13 +0300

    add y label (UNTESTED)

--- ecg.py
+++ ecg.py
@@ -8,24 +8,25 @@ import pathlib
 
 def render_png(ecg, file_path):
     ecg_arr = np.array(ecg)
-    abs_max_ecg = abs(ecg_arr).max()
     ecg_arr = np.array_split(np.trim_zeros(ecg_arr), 5)
     fig, axes = plt.subplots(len(ecg_arr), 1,
                              gridspec_kw={'hspace': 0.1},
                              figsize=(16.6, 23.4))
+
+    time = 12
+
     for i, (ax, ecg_row) in enumerate(zip(axes, ecg_arr)):
         plt.setp(ax.spines.values(), color='none')
-        ax.set_xticklabels([])
+        #ax.set_xticklabels([])
         ax.set_yticklabels([])
-        ax.xaxis.set_ticks_position('none')
+        #ax.xaxis.set_ticks_position('none')
         ax.yaxis.set_ticks_position('none')
-        a, b = i * len(ecg_row), (i + 1) * len(ecg_row)
-        ax.set_ylim(-abs_max_ecg, abs_max_ecg)
-        ax.set_xlim(a, b)
+        ax.set_ylim(-3000, 3000)
+        ax.set_xlim(time * i, time * (i + 1))
         ax.grid(which='both', linewidth='0.5', color=(1, 0.7, 0.7))
         ax.minorticks_on()
-        ax.plot(np.arange(a, b), ecg_row)
-    fig.savefig(file_path)
+        ax.plot(np.linspace(time * i, time * (i + 1), len(ecg_row)), ecg_row)
+    fig.savefig(file_path, bbox_inches='tight')
 
 
 def _test_render_png():
@@ -33,7 +34,11 @@ def _test_render_png():
     ecg = ecg_example.ecg
     file_path = 'files/example.png'
     png_path = pathlib.Path(file_path)
-    png_path.unlink(missing_ok=True)
+
+    try:
+        png_path.unlink()
+    except:
+        pass
 
     # Act
     render_png(ecg, file_path)
