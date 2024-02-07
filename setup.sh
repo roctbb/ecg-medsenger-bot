c522c1bded051006a5eac48df4cc93c39682353f
roctbb <roctbb@gmail.com>
2024-02-07 14:49:17 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 sudo pip3 install -r requirements.txt
+sudo cp ecg.ini /etc/uwsgi/apps/
 sudo cp agents_ecg.conf /etc/supervisor/conf.d/
 sudo cp agents_ecg_nginx.conf /etc/nginx/sites-enabled/
 sudo supervisorctl update
